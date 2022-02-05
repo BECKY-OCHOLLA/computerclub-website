beb61cd82fd9cc7b03bf5ee99879448b659b8a21
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-02-05 13:56:44 +0300

    button function:this will make the button functional

---
+++ index.js
@@ -0,0 +1,9 @@
+function myButton(){
+   //code here
+   //document.write ("Hey there");
+   var correct = 0;
+   var currentAnswer = document.first.quiz1.value;
+   if (currentAnswer==="Null"){correct++};
+   document.write(correct);
+   
+}

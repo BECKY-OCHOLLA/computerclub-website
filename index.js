ad576b73ed7786cc577d06ee58ab1e2952fb6088
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-02-05 15:06:45 +0300

    it is going to add marks to the exams

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ function myButton(){
    //code here
    //document.write ("Hey there");
    var score = 0;
-   var myAnswer = document.init.quiz1.value;
-   var myAnswer = document.init.quiz2.value;
-   var myAnswer = document.init.quiz3.value;
-   var myAnswer = document.init.quiz4.value;
-   var myAnswer = document.init.quiz5.value;
-   if (myAnswer==="Null"){score+=20};
+   var myAnswer1 = document.init.quiz1.value;
+   var myAnswer2 = document.init.quiz2.value;
+   var myAnswer3 = document.init.quiz3.value;
+   var myAnswer4 = document.init.quiz4.value;
+   var myAnswer5 = document.init.quiz5.value;
+   if (myAnswer1==="Null"){score+=20};
+   if (myAnswer2===";"){score+=20};
+   if (myAnswer3==="%"){score+=20};
+   if (myAnswer4==="All the words must start with a capital letter"){score+=20};
+   if (myAnswer5==="Addition"){score+=20};
+
+
+
+
    document.write(score);
    
 }

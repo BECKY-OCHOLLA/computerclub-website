b5a2a9b625db8f7e11e59871f83a8cd36c82b760
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-02-05 14:38:48 +0300

    it will add questions to website

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function myButton(){
    //code here
    //document.write ("Hey there");
-   var correct = 0;
-   var currentAnswer = document.first.quiz1.value;
-   if (currentAnswer==="Null"){correct++};
-   document.write(correct);
+   var score = 0;
+   var myAnswer = document.init.quiz1.value;
+   var myAnswer = document.init.quiz2.value;
+   var myAnswer = document.init.quiz3.value;
+   var myAnswer = document.init.quiz4.value;
+   var myAnswer = document.init.quiz5.value;
+   if (myAnswer==="Null"){score+=20};
+   document.write(score);
    
 }
